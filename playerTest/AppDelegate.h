eb4c7f75aa4d5ff2a3db7d0c97807d658337cf15
wangning <316835453@qq.com>
2016-08-16 09:35:53 +0800

    Initial Commit

---
+++ playerTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  playerTest
+//
+//  Created by 汪宁 on 16/8/16.
+//  Copyright © 2016年 ZHENAI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ playerTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  playerTest
+//
+//  Created by 汪宁 on 16/8/16.
+//  Copyright © 2016年 ZHENAI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
